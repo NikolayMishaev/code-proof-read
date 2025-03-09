d4d4297f7811fde6efa5d61dc0f08a02c1017571
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-03-09 18:55:55 +0300

    feat: добавлена проверка на точку в конце предложнеия

--- main.js
+++ main.js
@@ -18,16 +18,18 @@
 
 псевдокод:
     заменить в строке все вхождения "ie" на "ei"
-сделать первую букву заглавной
-проверить, что в конце точка, если нет, добавить ее
+    сделать первую букву заглавной
+    проверить, что в конце точка, если нет, добавить ее
 
 */
 
 function proofread (str) {
     str = str.toLowerCase().replaceAll('ie' ,'ei')
+    if (str.slice(-1) !== '.') str = str + '.'
     let firstLetter = str[0].toUpperCase()
     return firstLetter + str.slice(1)
 
 }
 
 console.log(proofread("He haD iEght ShOTs of CAffIEne."))
+console.log(proofread("He haD iEght ShOTs of CAffIEne"))
